64264b081189466509362b79b76a9687a54e624d
Dmitrii Lapin <6081986@gmail.com>
2017-10-03 23:46:33 +0300

    Упражнение на типы данных

---
+++ types.py
@@ -0,0 +1,26 @@
+# int
+int_num = 13
+print(int_num)
+
+int_num = 100_000_000
+print(int_num)
+print(type(int_num))
+
+# float
+float_num = 13.4
+print(float_num)
+print(type(float_num))
+
+float_num = 1.5e2
+print(float_num)
+
+# complex
+complex_num = 14 + 1j
+print(type(complex_num))
+print(complex_num.real)
+print(complex_num.imag)
+
+# none
+answer = None
+print(answer)
+print(bool(answer))
